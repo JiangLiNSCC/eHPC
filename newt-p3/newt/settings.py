6ffbce82dcce25082b9bab654036404743ff11cf
lijianggs@126.com <lijiang2014>
2016-10-19 15:15:53 +0800

    add celery-redis

--- newt-p3/newt/settings.py
+++ newt-p3/newt/settings.py
@@ -141,6 +141,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    # for django-celery
+    'djcelery',
 )
 
 AUTHENTICATION_BACKENDS = (
